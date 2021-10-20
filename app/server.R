7ffe65a543de342ef4b723f201924538f40fc5c1
Yuming-Tu <77839467+Yuming-Tu@users.noreply.github.com>
2021-10-20 18:05:04 -0400

    Add files via upload

--- app/server.R
+++ app/server.R
@@ -1,204 +1,235 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-###############################Install Related Packages #######################
-if (!require("shiny")) {
-    install.packages("shiny")
-    library(shiny)
-}
-if (!require("leaflet")) {
-    install.packages("leaflet")
-    library(leaflet)
-}
-if (!require("leaflet.extras")) {
-    install.packages("leaflet.extras")
-    library(leaflet.extras)
-}
-if (!require("dplyr")) {
-    install.packages("dplyr")
-    library(dplyr)
-}
-if (!require("magrittr")) {
-    install.packages("magrittr")
-    library(magrittr)
-}
-if (!require("mapview")) {
-    install.packages("mapview")
-    library(mapview)
-}
-if (!require("leafsync")) {
-    install.packages("leafsync")
-    library(leafsync)
-}
+setwd('C:\\Users\\tuyum\\Desktop\\study\\Columbia\\2021fall\\Applied DS\\project2')
+library(dplyr)
+library(lubridate)
+library(tidyverse)
+library(ggplot2)
+#dag data processing
+df <- read.csv("C:\\Users\\tuyum\\Desktop\\study\\Columbia\\2021fall\\Applied DS\\project2\\dog\\NYC_Dog_Licensing_Dataset.csv")
+df <- df[,-c(1,6,10,11)]
+
+#transfer to date dtype
+df$LicenseIssuedDate<-as.Date(df$LicenseIssuedDate,"%m/%d/%Y")
+df$LicenseExpiredDate<-as.Date(df$LicenseExpiredDate,"%m/%d/%Y")
+
+#We define the covid date as 2020-03-01; 
+#Here df1 only contains pre-covid dogs
+#dogs which have multiple entries, take the earliest issued date
+df1 <- df %>% 
+  group_by(AnimalName,AnimalGender,AnimalBirthMonth,BreedName) %>% 
+  slice(which.min(LicenseIssuedDate)) %>%
+  filter( LicenseIssuedDate < as.Date("2020-03-01"))
+#make old dog list
+old_dogs1 <- right_join(df,df1,by = c("AnimalName"="AnimalName",
+                                      "AnimalGender"="AnimalGender",
+                                      "AnimalBirthMonth"="AnimalBirthMonth",
+                                      "BreedName"="BreedName","ZipCode"="ZipCode"))[,-c(8,9)]
+
+#dogs registed before covid
+old_dogs2 <- df %>% filter(LicenseIssuedDate < as.Date("2020-03-01"))
+
+#new dogs registed since covid
+new_dogs <- anti_join(df,old_dogs1)
+new_dogs <- anti_join(new_dogs,old_dogs2)%>% 
+  group_by(AnimalName,AnimalGender,AnimalBirthMonth,BreedName,ZipCode) %>% 
+  slice(which.min(LicenseIssuedDate))
+
+#dogs registered from 2019/03-2020/03
+old_dogs <- df %>% filter(LicenseIssuedDate < as.Date("2020-03-01") & LicenseIssuedDate > as.Date("2019-03-01"))
+
+#dogs registered before 2019/03
+df1 <- df %>% filter(LicenseIssuedDate < as.Date("2019-03-01")) %>% 
+  group_by(AnimalName,AnimalGender,AnimalBirthMonth,BreedName) %>% 
+  slice(which.min(LicenseIssuedDate))
+#make before 19/03 old dog list
+old_dogs1 <- right_join(df,df1,by = c("AnimalName"="AnimalName","AnimalGender"="AnimalGender","AnimalBirthMonth"="AnimalBirthMonth","BreedName"="BreedName","ZipCode"="ZipCode"))[,-c(8,9)]
+
+#anti-join old_dog
+old_dogs <- anti_join(old_dogs,old_dogs1)
+
+
+#read in geo-zip file
+zip<-read.csv('C:\\Users\\tuyum\\Desktop\\study\\Columbia\\2021fall\\Applied DS\\project2\\dog\\zc_geo.csv',sep=";")
+new_dogs<-left_join(new_dogs,zip,by= c("ZipCode" = "Zip"))%>%
+  drop_na()
+
+old_dogs<-left_join(old_dogs,zip,by= c("ZipCode" = "Zip"))%>%
+  drop_na()
+
+
+#Emergency visits:
 
 #Data Processing
-total_citi_bike_df = read.csv('../data/citibike_data.csv')
-##compute the daily in and out difference for the station
-total_citi_bike_df$day_diff = total_citi_bike_df$endcount - total_citi_bike_df$startcount
-#assign each column to weekend or weekday
-total_citi_bike_df$weekend_or_weekday = ifelse(total_citi_bike_df$weekday %in% c('Saturday','Sunday'), "Weekend", "Weekday")
 
-#station info
-citi_bike_station_info <- total_citi_bike_df[,c('station_id','station_name','station_longitude','station_latitude')]
-#remove the duplicates based on station id 
-citi_bike_station_info <- citi_bike_station_info[!duplicated(citi_bike_station_info[ , c("station_id")]),]
+data<-read.csv('C:/Users/tuyum/Desktop/study/Columbia/2021fall/Applied DS/project2/emergency/Emergency_Department_Visits_and_Admissions_for_Influenza-like_Illness_and_or_Pneumonia.csv')
+zip<-read.csv('C:/Users/tuyum/Desktop/study/Columbia/2021fall/Applied DS/project2/emergency/zc_geo.csv',sep=";")
+zip<-zip%>%
+  mutate(mod_zcta=Zip)
+set.seed(0)
+data<-data[sample(nrow(data),200000),]
+data<-left_join(data,zip,by="mod_zcta")%>%
+  drop_na()
 
-#split the bike data to pre-covid and covid time period
-citi_bike_pre_covid_df = total_citi_bike_df[difftime(total_citi_bike_df$date,"2019-05-31")<=0,] #2019-05-01 ~ 2019-05-31
-citi_bike_covid_df = total_citi_bike_df[difftime(total_citi_bike_df$date,"2020-04-30")>=0,] #2020-05-01 ~ 2021-05-31
+data1<-data%>%
+  select(-extract_date)
+
+data1$date<-as.Date(data1$date,"%m/%d/%Y")
+data1<-data1%>%
+  arrange(date)
+Sys.setlocale("LC_TIME", "English")
+
+weekdays1 <- c('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday')
+data1$days <- factor((weekdays(data1$date) %in% weekdays1), 
+                     levels=c(FALSE, TRUE), labels=c('weekend', 'weekday'))
+
+a<-as.Date("2020-03-01")
+b<-as.Date("2020-09-30")
+c<-as.Date("2021-02-28")
+d<-as.Date("2021-10-31")
+head(data1)
+data1 <- data1 %>% mutate(seasons = ifelse(date >= a & date <= b,'20-Spring & Summer',
+                                           ifelse(date > b & date <= c,'20-Autumn & Winter',
+                                                  '21-Spring & Summer')))
+emergency_info <- zip[,c('Zip','Latitude','Longitude')]
 
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
-    ## Map Tab section
+  
+  output$Dog_left_map <- renderLeaflet({
+    #adjust for weekday/weekend effect
+    if (input$Genders =='Overall') {
+      leaflet_plt_df <- old_dogs %>% 
+        group_by(ZipCode) %>%
+        summarise(total_count = n()
+        ) %>% left_join(zip,by= c("ZipCode" = "Zip"))
+      
+    } else {
+      leaflet_plt_df <- old_dogs %>% 
+        filter(AnimalGender == as.character(input$Genders)) %>%
+        group_by(ZipCode) %>%
+        summarise(total_count = n()) %>% 
+        left_join(zip,by= c("ZipCode" = "Zip"))
+      
+    }
+    map_2019 <- leaflet_plt_df %>%
+      leaflet(options = leafletOptions(minZoom = 11, maxZoom = 13)) %>%
+      addTiles() %>%
+      addProviderTiles("CartoDB.Positron",
+                       options = providerTileOptions(noWrap = TRUE)) %>%
+      setView(-73.9834,40.7504,zoom = 12)
     
-    output$left_map <- renderLeaflet({
+    map_2019 %>%
+      addHeatmap(
+        lng=~Longitude,
+        lat=~Latitude,
+        intensity=~total_count,
+        max=700,
+        radius=16,
+        blur=8)
+    
+  }) #left map plot
+  
+  output$Dog_right_map <- renderLeaflet({
+    
+    #adjust for weekday/weekend effect
+    if (input$Genders =='Overall') {
+      leaflet_plt_df <- new_dogs %>% 
+        group_by(ZipCode) %>%
+        summarise(total_count = n()
+        )%>% left_join(zip,by= c("ZipCode" = "Zip"))
+      
+    } else {
+      leaflet_plt_df <- new_dogs %>% 
+        filter(AnimalGender == as.character(input$Genders)) %>%
+        group_by(ZipCode) %>%
+        summarise(total_count = n())%>% 
+        left_join(zip,by= c("ZipCode" = "Zip"))
+    }
+    #initial the map to plot on
+    map_2020 <- leaflet_plt_df %>%
+      leaflet(options = leafletOptions(minZoom = 11, maxZoom = 13)) %>%
+      addTiles() %>%
+      addProviderTiles("CartoDB.Positron",
+                       options = providerTileOptions(noWrap = TRUE)) %>%
+      setView(-73.9834,40.7504,zoom = 12)
+    
+    map_2020 %>%
+      addHeatmap(
+        lng=~Longitude,
+        lat=~Latitude,
+        intensity=~total_count,
+        max=700,
+        radius=16,
+        blur=8)
+    
+    
+    
+  }) #right map plot
+  
+  output$Emergency<- renderLeaflet({
     
     #adjust for weekday/weekend effect
     if (input$adjust_time =='Overall') {
-        leaflet_plt_df <- citi_bike_pre_covid_df %>% 
-                            group_by(station_id) %>%
-                            summarise(total_start_count = sum(startcount),
-                                      total_end_count = sum(endcount),
-                                      total_day_diff = sum(day_diff),
-                                      total_diff_percentage = sum(day_diff)/sum(startcount),
-                            ) %>% left_join(citi_bike_station_info,by='station_id')
+      leaflet_plt_df <- data1 %>% 
+        group_by(Zip) %>%
+        summarise(
+          total=sum(total_ed_visits),
+          total_pne = sum(ili_pne_visits),
+          total_pneadm = sum(ili_pne_admissions),
+          
+          adm_percentage=sum(ili_pne_admissions)/sum(ili_pne_visits)
+        ) %>% left_join(emergency_info,by='Zip')
     } else {
-        leaflet_plt_df <- citi_bike_pre_covid_df %>% 
-                            filter(weekend_or_weekday == input$adjust_time) %>%
-                            group_by(station_id) %>%
-                                summarise(total_start_count = sum(startcount),
-                                          total_end_count = sum(endcount),
-                                          total_day_diff = sum(day_diff),
-                                          total_diff_percentage = sum(day_diff)/sum(startcount),
-                                ) %>% left_join(citi_bike_station_info,by='station_id')
-                            } 
-
-        
-    map_2019 <- leaflet_plt_df %>%
-         leaflet(options = leafletOptions(minZoom = 11, maxZoom = 13)) %>%
-         addTiles() %>%
-         addProviderTiles("CartoDB.Positron",
-                          options = providerTileOptions(noWrap = TRUE)) %>%
-         setView(-73.9834,40.7504,zoom = 12)
-     
-     if (input$adjust_score == 'start_cnt') {
-         map_2019 %>%
-             addHeatmap(
-                        lng=~station_longitude,
-                        lat=~station_latitude,
-                        intensity=~total_start_count,
-                        max=4000,
-                        radius=8,
-                        blur=10)
-     }else if (input$adjust_score == 'end_cnt') {
-         map_2019 %>%
-             addHeatmap(
-                        lng=~station_longitude,
-                        lat=~station_latitude,
-                        intensity=~total_end_count,
-                        max=4000,
-                        radius=8,
-                        blur=10)
-     } else if (input$adjust_score == 'day_diff_absolute'){
-         map_2019 %>%
-             addHeatmap(
-                        lng=~station_longitude,
-                        lat=~station_latitude,
-                        intensity=~total_day_diff,
-                        max=50,
-                        radius=8,
-                        blur=10)
-         
-     }else if (input$adjust_score == 'day_diff_percentage'){
-         map_2019 %>%
-             addHeatmap(
-                        lng=~station_longitude,
-                        lat=~station_latitude,
-                        intensity=~total_diff_percentage,#change to total day diff percentage
-                        max=0.1,
-                        radius=8,
-                        blur=10)
-         
-     }
-     }) #left map plot
+      leaflet_plt_df <- data1 %>% 
+        filter(days == input$adjust_time) %>%
+        group_by(Zip) %>%
+        summarise(
+          total=sum(total_ed_visits),
+          total_pne = sum(ili_pne_visits),
+          total_pneadm = sum(ili_pne_admissions),
+          
+          adm_percentage=sum(ili_pne_admissions)/sum(ili_pne_visits)
+        ) %>% left_join(emergency_info,by='Zip')
+    } 
     
-    output$right_map <- renderLeaflet({
-        #adjust for weekday/weekend effect
-        if (input$adjust_time =='Overall') {
-            leaflet_plt_df <- citi_bike_covid_df %>% 
-                group_by(station_id) %>%
-                summarise(total_start_count = sum(startcount),
-                          total_end_count = sum(endcount),
-                          total_day_diff = sum(day_diff),
-                          total_diff_percentage = sum(day_diff)/sum(startcount),
-                ) %>% left_join(citi_bike_station_info,by='station_id')
-        } else {
-            leaflet_plt_df <- citi_bike_covid_df %>% 
-                filter(weekend_or_weekday == input$adjust_time) %>%
-                group_by(station_id) %>%
-                summarise(total_start_count = sum(startcount),
-                          total_end_count = sum(endcount),
-                          total_day_diff = sum(day_diff),
-                          total_diff_percentage = sum(day_diff)/sum(startcount),
-                ) %>% left_join(citi_bike_station_info,by='station_id')
-        } 
-        #initial the map to plot on
-        map_2020 <- leaflet_plt_df %>%
-            leaflet(options = leafletOptions(minZoom = 11, maxZoom = 13)) %>%
-            addTiles() %>%
-            addProviderTiles("CartoDB.Positron",
-                             options = providerTileOptions(noWrap = TRUE)) %>%
-            setView(-73.9834,40.7504,zoom = 12) 
+    
+    map <- leaflet_plt_df %>%
+      leaflet(options = leafletOptions(minZoom = 11, maxZoom = 13)) %>%
+      addTiles() %>%
+      addProviderTiles("CartoDB.Positron",
+                       options = providerTileOptions(noWrap = TRUE)) %>%
+      setView(-73.9834,40.7504,zoom = 12)
+    
+    if (input$adjust_score == 'total visit') {
+      map %>%
         
-        if (input$adjust_score == 'start_cnt') {
-            map_2020 %>%
-                addHeatmap(
-                           lng=~station_longitude,
-                           lat=~station_latitude,
-                            intensity=~total_start_count, #change to total start count
-                            max=4000,
-                            radius=8,
-                           blur=10)
-        }else if (input$adjust_score == 'end_cnt') {
-            map_2020 %>%
-                addHeatmap(
-                           lng=~station_longitude,
-                           lat=~station_latitude,
-                           intensity=~total_end_count,#change to total end count
-                           max=4000,
-                           radius=8,
-                           blur=10)
-        } else if (input$adjust_score == 'day_diff_absolute'){
-            map_2020 %>%
-                addHeatmap(
-                           lng=~station_longitude,
-                           lat=~station_latitude,
-                           intensity=~total_day_diff,#change to total day diff
-                           max=50,
-                           radius=8,
-                           blur=10)
-            
-        }else if (input$adjust_score == 'day_diff_percentage'){
-            map_2020 %>%
-                addHeatmap(
-                           lng=~station_longitude,
-                           lat=~station_latitude,
-                           intensity=~total_diff_percentage,#change to total day diff percentage
-                           max=0.1,
-                           radius=8,
-                           blur=10)
-            
-        }
+        addHeatmap(
+          lng=~Longitude,
+          lat=~Latitude,
+          intensity=~total_pne,
+          max=4000,
+          radius=8,
+          blur=10)
+    }else if (input$adjust_score == 'admission') {
+      map %>%
         
-    }) #right map plot
-
+        addHeatmap(
+          lng=~Longitude,
+          lat=~Latitude,
+          intensity=~total_pneadm,
+          max=4000,
+          radius=8,
+          blur=10)
+    } else if (input$adjust_score == 'total'){
+      map %>%
+        
+        addHeatmap(
+          lng=~Longitude,
+          lat=~Latitude,
+          intensity=~total,
+          max=50,
+          radius=8,
+          blur=10)
+      
+    }
+  })#left map plot
 })
-
-
--- app/ui.R
+++ app/ui.R
@@ -19,56 +19,78 @@ if (!require("leaflet.extras")) {
   install.packages("leaflet.extras")
   library(leaflet.extras)
 }
-
+#!diagnostics off
 # Define UI for application that draws a histogram
 shinyUI(
-    navbarPage(strong("Citi Bike Study",style="color: white;"), 
-               theme=shinytheme("cerulean"), # select your themes https://rstudio.github.io/shinythemes/
-#------------------------------- tab panel - Maps ---------------------------------
-                tabPanel("Maps",
-                         icon = icon("map-marker-alt"), #choose the icon for
-                         div(class = 'outer',
-                        # side by side plots
-                        fluidRow(
-                                splitLayout(cellWidths = c("50%", "50%"), 
-                                             leafletOutput("left_map",width="100%",height=1200),
-                                             leafletOutput("right_map",width="100%",height=1200))),
-                        #control panel on the left
-                        absolutePanel(id = "control", class = "panel panel-default", fixed = TRUE, draggable = TRUE,
-                                      top = 200, left = 50, right = "auto", bottom = "auto", width = 250, height = "auto",
-                                      tags$h4('Citi Bike Activity Comparison'), 
-                                      tags$br(),
-                                      tags$h5('Pre-covid(Left) Right(Right)'), 
-                                      prettyRadioButtons(
-                                                      inputId = "adjust_score",
-                                                      label = "Score List:", 
-                                                      choices = c("start_cnt", 
-                                                                  "end_cnt", 
-                                                                  "day_diff_absolute",
-                                                                  "day_diff_percentage"),
-                                                      inline = TRUE, 
-                                                      status = "danger",
-                                                      fill = TRUE
-                                                        ),
-                                      awesomeRadio("adjust_time", 
-                                                   label="Time",
-                                                    choices =c("Overall",
-                                                               "Weekday", 
-                                                               "Weekend"), 
-                                                    selected = "Overall",
-                                                    status = "warning"),
-                                      # selectInput('adjust_weather',
-                                      #             label = 'Adjust for Weather',
-                                      #             choices = c('Yes','No'), 
-                                      #             selected = 'Yes'
-                                      #             ),
-                                      style = "opacity: 0.80"
-                                      
-                                ), #Panel Control - Closing
-                            ) #Maps - Div closing
-                        ) #tabPanel maps closing
-   
-
-
-    ) #navbarPage closing  
-) #Shiny UI closing    
+  navbarPage(strong("NYC Covid Research ",style="color: white;"), 
+             theme=shinytheme("cerulean"), # select your themes https://rstudio.github.io/shinythemes/
+             #------------------------------- tab panel - Maps ---------------------------------
+             tabPanel("Dog licenses",icon = icon("map-marker-alt"), #choose the icon for
+                      
+                      div(class = 'outer',
+                          # side by side plots
+                          fluidRow(splitLayout(cellWidths = c("50%", "50%"),
+                                               leafletOutput("Dog_left_map",width="100%",height=1200),
+                                               leafletOutput("Dog_right_map",width="100%",height=1200))),
+                          #control panel on the left
+                          absolutePanel(id = "control", class = "panel panel-default", 
+                                        fixed = TRUE, draggable = TRUE, top = 200, left = 50, 
+                                        right = "auto", bottom = "auto", width = 250, height = "auto",
+                                        tags$h4('Dog Licenses Registered '),
+                                        tags$br(),tags$h5('Sorts'), 
+                                        #prettyRadioButtons(inputId = "Genders",
+                                        #label = "Genders",
+                                        #choices = c("Female", "Male"),
+                                        #inline = TRUE, status = "danger",
+                                        #fill = TRUE),
+                                        awesomeRadio("Genders", label="Pick Desired Gender",
+                                                     choices =c("Overall", "F", "M"),
+                                                     selected = "Overall",status = "danger"),
+                                        style = "opacity: 0.80"
+                                        
+                          ), #Panel Control - Closing
+                      ) #Maps - Div closing
+             ), #tabPanel maps closing
+             tabPanel("Emergency department visits",
+                      icon = icon("map-marker-alt"), #choose the icon for
+                      div(class = 'outer',
+                          # side by side plots
+                          fluidRow(
+                            splitLayout(cellWidths = "100%", 
+                                        leafletOutput('Emergency',width="100%",height=1200))),
+                          #control panel on the left
+                          absolutePanel(id = "control", class = "panel panel-default", fixed = TRUE, draggable = TRUE,
+                                        top = 200, left = 50, right = "auto", bottom = "auto", width = 250, height = "auto",
+                                        tags$h4('Emergency Department Visits '), 
+                                        tags$br(),
+                                        tags$h5('Sorts'), 
+                                        prettyRadioButtons(
+                                          inputId = "adjust_score",
+                                          label = "Score List:", 
+                                          choices = c("total visit", 
+                                                      "admission", 
+                                                      "total"),
+                                          inline = TRUE, 
+                                          status = "danger",
+                                          fill = TRUE
+                                        ),
+                                        awesomeRadio("adjust_time", 
+                                                     label="Time",
+                                                     choices =c("Overall",
+                                                                "weekday", 
+                                                                "weekend"), 
+                                                     selected = "Overall",
+                                                     status = "warning"),
+                                        # selectInput('adjust_weather',
+                                        #             label = 'Adjust for Weather',
+                                        #             choices = c('Yes','No'), 
+                                        #             selected = 'Yes'
+                                        #             ),
+                                        style = "opacity: 0.80"
+                                        
+                          ) #Panel Control - Closing
+                      ) #Maps - Div closing
+             ) #tabPanel maps closing
+             
+  ) #navbarPage closing  
+) #Shiny UI closing   
